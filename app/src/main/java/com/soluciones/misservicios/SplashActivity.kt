149f541a8b1114b7a7673b9cfdbed7e9da132cc6
Salva <svalladaresr@miumg.edu.gt>
2021-06-07 04:06:35 -0600

    animation

---
+++ app/src/main/java/com/soluciones/misservicios/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.soluciones.misservicios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.soluciones.misservicios.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+
+        setAnimation()
+    }
+
+    private fun setAnimation(){
+        binding.img.animate().translationY(-4000f).setDuration(800).startDelay = 6000
+        binding.logo.animate().translationY(3000f).setDuration(800).startDelay = 6000
+        binding.appName.animate().translationY(3000f).setDuration(800).startDelay = 6000
+        binding.lottie.animate().translationY(3000f).setDuration(800).startDelay = 6000
+    }
+}
